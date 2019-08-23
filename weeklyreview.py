b2f29871a406998b78717705718fbad12862e0ad
rjrobins16 <rjrobins16@gmail.com>
2019-08-23 16:30:21 -0500

    FINAL (ADDED MORE COMMENTS)

--- weeklyreview.py
+++ weeklyreview.py
@@ -1,7 +1,7 @@
 print ("Welcome to the BANK OF RACHEL.")
 
-accountHolderName = input("What is your name?")
-pin = int(input("Create a 4-digit pin number:"))
+accountHolderName = input("What is your name?") #prompts user for name
+pin = int(input("Create a 4-digit pin number:")) #prompts user for pin
 
 # PROMPTS USER FOR BANK OPTIONS:
 userInput = int(input((f' Please choose one of following options:\n'
@@ -10,6 +10,7 @@ userInput = int(input((f' Please choose one of following options:\n'
                    f'3) Withdraw money from account\n'
                    f'4) Quit\n'
                    f' > What will you like to do? \n')))
+#declares values and sets them zero!
 
 accountBalance = 0
 deposit = 0
@@ -19,11 +20,11 @@ withdrawal = 0
 while userInput != 4:
 
 
-    if userInput == 1:
+    if userInput == 1: #prints current balance
         print("CHECK BALANCE")
         print (f'Your account balance is ${accountBalance}.')
 
-    if userInput == 2:
+    if userInput == 2: #allows user to ADD funds
         print ("DEPOSIT FUNDS")
         deposit =int(input(f'How much would {accountHolderName} like to deposit into this account?'))
         accountBalance = accountBalance + deposit
@@ -31,8 +32,8 @@ while userInput != 4:
 ## CHECKS FOR PIN BEFORE ALLOWING USER TO WITHDRAW FUNDS
     if userInput ==3:
         print("WITHDRAW FUNDS")
-        pincheck = int(input("What is your 4-digit pin?"))
-        while pincheck != pin:
+        pincheck = int(input("What is your 4-digit pin?")) #asks user to verify their pin
+        while pincheck != pin: #checks to see if the pin entry matches
             print ("INCORRECT PIN. PLEASE TRY AGAIN.")
             pincheck = int(input("What is your 4-digit pin?"))
         else:
@@ -40,7 +41,7 @@ while userInput != 4:
             if withdrawal > accountBalance: #IF THE AMOUNT THEY WANT TO TAKE
                 # OUT IS MORE THAN THE BALANCE THEY CAN NOT REMOVE FUNDS
                 print("INSUFFICIENT FUNDS.")
-            else:
+            else: #if the account users balance supports the withdrawal then subtract and print the new balance
                 accountBalance = accountBalance - withdrawal
                 print (f'Your account balance is now ${accountBalance}.')
 
@@ -52,3 +53,5 @@ while userInput != 4:
                        f'3) Withdraw money from account\n'
                        f'4) Quit\n'
                        f' > What will {accountHolderName} like to do? \n')))
+
+#end of loop (and it works :)))))))
